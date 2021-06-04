f30053aa366f3ea1206fe846ba383a639359ef70
Pival <valentin.l-homel@epitech.eu>
2021-06-04 10:25:46 +0200

    Add constructeurs et methodes à EuclidEngineArea

--- Wrapper_Project/Assets/EuclidEngine.cs
+++ Wrapper_Project/Assets/EuclidEngine.cs
@@ -8,131 +8,19 @@ using System.Runtime.InteropServices;
 
 public class EuclidEngine
 {
+#if UNITY_IOS
+    public static string plugin = "__Internal";
+#else
+    public static string plugin = "EEPluginCpp";
+#endif
+
     public static Func<int, UnityEngine.Object> FindObjectFromInstanceID = null;
+
     static EuclidEngine()
     {
         FindObjectFromInstanceID = (Func<int, UnityEngine.Object>)Delegate.CreateDelegate(
             typeof(Func<int, UnityEngine.Object>),
             typeof(UnityEngine.Object).GetMethod("FindObjectFromInstanceID",
                 System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static));
-    }
-
-#if UNITY_IOS
-    private const string eePlugin = "__Internal";
-#else
-    private const string eePlugin = "EEPluginCpp";
-#endif
-
-    /* 
-    * @brief Create a new NEA, with given dimension
-    * 
-    * @param sizeX The size of the object on the X coordinate
-    * @param sizeY The size of the object on the Y coordinate
-    * @param sizeZ The size of the object on the Z coordinate
-    * @param internalSizeX The size of the area inside the object on the X coordinate
-    * @param internalSizeY The size of the area inside the object on the Y coordinate
-    * @param internalSizeZ The size of the area inside the object on the Z coordinate
-    */
-    [DllImport(eePlugin)] public static extern IntPtr EEAreaCreate(double iX, double iY, double iZ, double eX, double eY, double eZ);
-
-    /*
-     * @brief Destroy the Area object
-    */
-    [DllImport(eePlugin)] public static extern void EEAreaDelete(IntPtr area);
-
-    /*
-    * @brief Set the size of the NEA
-    * 
-    * @details Adjusting the size of the area may lead to insertion of new object inside the NEA.
-    * However, as object inside the NEA are anchored relatively to their internal position, they will stay at the border of the NEA, as this will only change the external size of the NEA.
-    *
-    * @param Area object
-    * @param sizeX The new size of the object on the X coordinate
-    * @param sizeY The new size of the object on the Y coordinate
-    * @param sizeZ The new size of the object on the Z coordinate
-    */
-    [DllImport(eePlugin)] public static extern void EEAreaSetSize(IntPtr area, double pExternX, double pExternY, double pExternZ);
-
-    /*
-    * @brief Set the size of the area inside the NEA
-    * 
-    * @details Adjusting the internal size of the area may lead change of position for object and relaese of object inside the NEA.
-    * 
-    * @param Area object
-    * @param sizeX The new size of the area inside the object on the X coordinate
-    * @param sizeY The new size of the area inside the object on the Y coordinate
-    * @param sizeZ The new size of the area inside the object on the Z coordinate
-    */
-    [DllImport(eePlugin)] public static extern void EEAreaSetInternalSize(IntPtr area, double pInternX, double pInternY, double pInternZ);
-
-    /*
-    * @brief Set the size of the transition area around the object
-    * 
-    * @details The size will be applied on both side of the object, relatively to the coordinates (right and left, top and bottom, front and back)
-    * Behaviour of object inside the transition area is undefined in case of changement of its size.
-    * 
-    * @param sizeX The new size of the transit area of the object on the X coordinate
-    * @param sizeY The new size of the transit area of the object on the Y coordinate
-    * @param sizeZ The new size of the transit area of the object on the Z coordinate
-    */
-    [DllImport(eePlugin)] public static extern void EEAreaSetTransitAreaSize(IntPtr area, double sizeX, double sizeY, double sizeZ);
-
-    /*
-    * @brief Add a new object inside the NEA.
-    * 
-    * @details The object given as argument will be the one used whenever a callback need to be called.
-    * 
-    * @param Area object
-    * @param id A unique identifier for the object
-    * @param object The object which enter inside the NEA
-    */
-    [DllImport(eePlugin)] public static extern void EEAreaAddObjectInside(IntPtr area, int objectId, IntPtr obj);
-
-    /*
-    * @brief Remove an object from inside the NEA.
-    * 
-    * @param Area object
-    * @param id The unique identifier identifying the object`
-    */
-    [DllImport(eePlugin)] public static extern void EEAreaRemoveObjectInside(IntPtr area, int objectId);
-
-    /*
-    * @brief Update the objects inside the NEA. Need to be called each frame.
-    * 
-    * @details Update objects inside the NEA relatively to changes occuring after the last frame.
-    * Most callback registered to the NEA will be called on this function, and here only.
-    */
-    [DllImport(eePlugin)] public static extern void EEAreaUpdate(IntPtr area);
-
-    public delegate void ScalerFn(IntPtr go, double x, double y, double z);
-    public delegate void PositionGetterFn(IntPtr go, ref double x, ref double y, ref double z);
-    public delegate void SizeGetterFn(IntPtr go, ref double minX, ref double minY, ref double minZ, ref double maxX, ref double maxY, ref double maxZ);
-    [DllImport(eePlugin)] public static extern void EEAreaSetAreaPositionGetterCallback(IntPtr area, PositionGetterFn callback);
-    [DllImport(eePlugin)] public static extern void EEAreaSetScalerCallback(IntPtr area, ScalerFn callback);
-    [DllImport(eePlugin)] public static extern void EEAreaSetPositionGetterCallback(IntPtr area, PositionGetterFn callback);
-    [DllImport(eePlugin)] public static extern void EEAreaSetSizeGetterCallback(IntPtr area, SizeGetterFn callback);
-
-    // [DllImport(eePlugin)] public static extern void EEAreaGetSize(IntPtr area, ref double x, ref double y, ref double z);
-
-    public static EuclidEngineArea CreateArea(double interiorX, double interiorY, double interiorZ, double exteriorX, double exteriorY, double exteriorZ)
-    {
-        IntPtr intPtr = EEAreaCreate(interiorX, interiorY, interiorZ, exteriorX, exteriorY, exteriorZ);
-
-        GameObject obj = new GameObject();
-
-        EuclidEngineArea area = obj.AddComponent<EuclidEngineArea>();
-        area._area = intPtr;
-        area.SetExternalSize(exteriorX, exteriorY, exteriorZ);
-        return area;
-    }
-
-    public static EuclidEngineArea CreateArea(double exteriorX, double exteriorY, double exteriorZ)
-    {
-        return CreateArea(1.0f, 1.0f, 1.0f, exteriorX, exteriorY, exteriorZ);
-    }
-
-    public static void DeleteArea(EuclidEngineArea area)
-    {
-        EEAreaDelete(area._area);
     }
 }
--- Wrapper_Project/Assets/EuclidEngineArea.cs
+++ Wrapper_Project/Assets/EuclidEngineArea.cs
@@ -9,23 +9,70 @@ using System.Runtime.InteropServices;
 [RequireComponent(typeof(BoxCollider), typeof(Rigidbody))]
 public class EuclidEngineArea : MonoBehaviour
 {
-    public IntPtr _area;
-    private EuclidEngine.PositionGetterFn _areaPosGetter;
-    private EuclidEngine.PositionGetterFn _posGetter;
-    private EuclidEngine.SizeGetterFn _sizeGetter;
-    private EuclidEngine.ScalerFn _scaler;
+    /************************************************/
+    /*                                              */
+    /*           C++ functions prototype            */
+    /*                                              */
+    /************************************************/
 
+    [DllImport(EuclidEngine.plugin)] private static extern IntPtr EEAreaCreate(double iX, double iY, double iZ, double eX, double eY, double eZ);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaDelete(IntPtr area);
+        private delegate void PositionGetterFn(IntPtr go, ref double x, ref double y, ref double z);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaSetAreaPositionGetterCallback(IntPtr area, PositionGetterFn callback);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaSetPositionGetterCallback(IntPtr area, PositionGetterFn callback);
+        private delegate void SizeGetterFn(IntPtr go, ref double minX, ref double minY, ref double minZ, ref double maxX, ref double maxY, ref double maxZ);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaSetSizeGetterCallback(IntPtr area, SizeGetterFn callback);
+        private delegate void ScalerFn(IntPtr go, double x, double y, double z);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaSetScalerCallback(IntPtr area, ScalerFn callback);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaSetSize(IntPtr area, double pExternX, double pExternY, double pExternZ);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaSetInternalSize(IntPtr area, double pInternX, double pInternY, double pInternZ);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaSetTransitAreaSize(IntPtr area, double sizeX, double sizeY, double sizeZ);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaAddObjectInside(IntPtr area, int objectId, IntPtr obj);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaRemoveObjectInside(IntPtr area, int objectId);
+    [DllImport(EuclidEngine.plugin)] private static extern void EEAreaUpdate(IntPtr area);
+
+
+    /************************************************/
+    /*                                              */
+    /*                   Variables                  */
+    /*                                              */
+    /************************************************/
+
+    // C++ Area object and callback
+    private IntPtr _area;
+    private PositionGetterFn _areaPosGetter;
+    private PositionGetterFn _posGetter;
+    private SizeGetterFn _sizeGetter;
+    private ScalerFn _scaler;
+
+    // C# Area object
+    private BoxCollider _collider;
+
+    // Area variables
+    private Vector3 _size = new Vector3(1, 1, 1);
+    private Vector3 _internalSize = new Vector3(1, 1, 1);
+    private Vector3 _transitSize = new Vector3(0, 0, 0);
+
+
+    /************************************************/
+    /*                                              */
+    /*             Unity events handler             */
+    /*                                              */
+    /************************************************/
+
+    //Called in editor, when creating object
     private void Reset()
     {
         var boxCollider = GetComponent<BoxCollider>();
         var rigidbody = GetComponent<Rigidbody>();
 
         boxCollider.isTrigger = true;
-        
+
         rigidbody.isKinematic = true;
         rigidbody.useGravity = false;
     }
 
+    //Called on launch
     void Awake()
     {
         _areaPosGetter = new EuclidEngine.PositionGetterFn(SetAreaPosition);
@@ -38,35 +85,161 @@ public class EuclidEngineArea : MonoBehaviour
         EuclidEngine.EEAreaSetScalerCallback(_area, _scaler);
     }
 
+    //Called on launch, after Awake
+    void Start()
+    {
+        _collider = GetComponent<BoxCollider>();
+    }
+
+    //Called at end (of object or scene)
     void OnDestroy()
     {
         EuclidEngine.EEAreaDelete(_area);
     }
 
+    //Called on collision
     void OnTriggerEnter(Collider c)
     {
         int id = c.GetInstanceID();
         EuclidEngine.EEAreaAddObjectInside(_area, id, (IntPtr)id);
     }
 
+    //Called at the end of collision
     void OnTriggerExit(Collider c)
     {
         EuclidEngine.EEAreaRemoveObjectInside(_area, c.GetInstanceID());
     }
 
+    //Called every frame
     void Update()
     {
         EuclidEngine.EEAreaUpdate(_area);
     }
 
-    void SetAreaPosition(IntPtr _, ref double x, ref double y, ref double z)
+
+    /************************************************/
+    /*                                              */
+    /*                 Constructors                 */
+    /*                                              */
+    /************************************************/
+
+    public static EuclidEngineArea Instantiate(Vector3 position = Vector3.zero, Quaternion rotation = Quaternion.identity)
+    { return EuclidEngineArea.Instantiate(Vector3.one, Vector3.one, Vector3.zero, position, rotation); }
+    public static EuclidEngineArea Instantiate(Vector3 position, Quaternion rotation, Transform parent)
+    { return EuclidEngineArea.Instantiate(Vector3.one, Vector3.one, Vector3.zero, position, rotation, parent); }
+
+    public static EuclidEngineArea Instantiate(Vector3 areaSize, Vector3 internalSize, Vector3 position = Vector3.zero, Quaternion rotation = Vector3.zero)
+    { return EuclidEngineArea.Instantiate(areaSize, internalSize, Vector3.zero, position, rotation); }
+    public static EuclidEngineArea Instantiate(Vector3 areaSize, Vector3 internalSize, Vector3 position, Quaternion rotation, Transform parent)
+    { return EuclidEngineArea.Instantiate(areaSize, internalSize, Vector3.zero, position, rotation, parent); }
+
+    public static EuclidEngineArea Instantiate(Vector3 areaSize, Vector3 internalSize, Vector3 transitionSize, Vector3 position = Vector3.zero, Quaternion rotation = Vector3.zero)
+    {
+        return EuclidEngineArea.Instantiate(areaSize, internalSize, transitionSize, position, rotation, null);
+    }
+
+    public static EuclidEngineArea Instantiate(Vector3 areaSize, Vector3 internalSize, Vector3 transitionSize, Vector3 position, Quaternion rotation, Transform parent)
+    {
+        GameObject go = new GameObject(typeof(EuclidEngineArea).ToString());
+        if (parent)
+            go.transform.parent = parent;
+        go.transform.localPosition = position;
+        go.transform.localRotation = rotation;
+
+        EuclidEngineArea area = go.AddComponent<EuclidEngineArea>();
+        area.SetSize(areaSize, internalSize, transitionSize);
+
+        return area;
+    }
+
+
+    /************************************************/
+    /*                                              */
+    /*              C# public functions             */
+    /*                                              */
+    /************************************************/
+
+    public Vector3 areaSize
+    {
+        get { return _size; }
+        set {
+            if (value.x <= 0 || value.y <= 0 || value.z <= 0)
+                throw new ArgumentOutOfRangeException("size", "Size must be strictly positiv"));
+            _size = value;
+            EEAreaSetSize(_area, (double)_size.x, (double)_size.z, (double)_size.y);
+            _collider.size = _size + 2 * _transitSize;
+        }
+    }
+
+    public Vector3 internalSize
+    {
+        get { return _internalSize; }
+        set {
+            if (value.x <= 0 || value.y <= 0 || value.z <= 0)
+                throw new ArgumentOutOfRangeException("internalSize", "Internal size must be strictly positiv"));
+            _internalSize = value;
+            EEAreaSetIntenalSize(_area, (double)_internalSize.x, (double)_internalSize.z, (double)_internalSize.y);
+        }
+    }
+
+    public Vector3 transitionSize
+    {
+        get { return _transitSize; }
+        set {
+            if (value.x < 0 || value.y < 0 || value.z < 0)
+                throw new ArgumentOutOfRangeException("transitSize", "Transition size cannot be negativ"));
+            _transitSize = value;
+            EEAreaSetTransitSize(_area, (double)_transitSize.x, (double)_transitSize.z, (double)_transitSize.y);
+            _collider.size = _size + 2 * _transitSize;
+        }
+    }
+
+    public Vector3 size
+    {
+        get { return _size + 2 * _transitSize; }
+    }
+
+    public void SetSize(Vector3 areaSize, Vector3 internalSize)
+    { SetSize(areaSize, internalSize, _transitSize); }
+    public void SetSize(Vector3 areaSize, Vector3 internalSize, Vector3 transitionSize)
+    {
+        if (areaSize.x <= 0 || areaSize.y <= 0 || areaSize.z <= 0)
+            throw new ArgumentOutOfRangeException("areaSize", "Size must be strictly positiv"));
+        if (internalSize.x <= 0 || internalSize.y <= 0 || internalSize.z <= 0)
+            throw new ArgumentOutOfRangeException("internalSize", "Internal size must be strictly positiv"));
+        if (transitionSize.x < 0 || transitionSize.y < 0 || transitionSize.z < 0)
+            throw new ArgumentOutOfRangeException("transitionSize", "Transition size cannot be negativ"));
+        _size = areaSize;
+        _internalSize = internalSize;
+        _transitSize = transitionSize;
+        EEAreaSetSize(_area, (double)_size.x, (double)_size.z, (double)_size.y);
+        EEAreaSetIntenalSize(_area, (double)_internalSize.x, (double)_internalSize.z, (double)_internalSize.y);
+        EEAreaSetTransitSize(_area, (double)_transitSize.x, (double)_transitSize.z, (double)_transitSize.y);
+        _collider.size = _size + 2 * _transitSize;
+    }
+
+
+    /************************************************/
+    /*                                              */
+    /*            C# private functions              */
+    /*                                              */
+    /************************************************/
+
+
+    /************************************************/
+    /*                                              */
+    /*                 C++ callbacks                */
+    /*                                              */
+    /************************************************/
+
+    private void SetAreaPosition(IntPtr _, ref double x, ref double y, ref double z)
     {
         x = transform.position.x;
         y = transform.position.y;
         z = transform.position.z;
     }
 
-    void SetObjectPosition(IntPtr go, ref double x, ref double y, ref double z)
+    private void SetObjectPosition(IntPtr go, ref double x, ref double y, ref double z)
     {
         Collider collider = EuclidEngine.FindObjectFromInstanceID((int)go) as Collider;
         x = collider.transform.position.x;
@@ -74,7 +247,7 @@ public class EuclidEngineArea : MonoBehaviour
         z = collider.transform.position.z;
     }
 
-    void SetObjectSize(IntPtr go, ref double minx, ref double miny, ref double minz, ref double maxx, ref double maxy, ref double maxz)
+    private void SetObjectSize(IntPtr go, ref double minx, ref double miny, ref double minz, ref double maxx, ref double maxy, ref double maxz)
     {
         Collider collider = EuclidEngine.FindObjectFromInstanceID((int)go) as Collider;
         minx = collider.bounds.min.x;
@@ -85,38 +258,10 @@ public class EuclidEngineArea : MonoBehaviour
         maxz = collider.bounds.max.z;
     }
 
-    void ScaleObjetct(IntPtr go, double x, double y, double z)
+    private void ScaleObjetct(IntPtr go, double x, double y, double z)
     {
         //print("Scale");
         Collider collider = EuclidEngine.FindObjectFromInstanceID((int)go) as Collider;
         collider.transform.localScale = new Vector3((float)x, (float)y, (float)z);
-    }
-
-    public void SetExternalSize(double x, double y, double z)
-    {
-        EuclidEngine.EEAreaSetSize(_area, x, z, y);
-
-        var boxCollider = GetComponent<BoxCollider>();
-        boxCollider.size = new Vector3((float)x, (float)y, (float)z);
-    }
-
-    public void SetTransitAreaSize(double sizeX, double sizeY, double sizeZ)
-    {
-        EuclidEngine.EEAreaSetTransitAreaSize(_area, sizeX, sizeY, sizeZ);
-    }
-
-    public void AddObjectInside(int objectId, EuclidEngineArea area)
-    {
-        EuclidEngine.EEAreaAddObjectInside(_area, objectId, area._area);
-    }
-
-    public void RemoveObjectInside(int objectId)
-    {
-        EuclidEngine.EEAreaRemoveObjectInside(_area, objectId);
-    }
-
-    public void AreaUpdate()
-    {
-        EuclidEngine.EEAreaUpdate(_area);
     }
 }
