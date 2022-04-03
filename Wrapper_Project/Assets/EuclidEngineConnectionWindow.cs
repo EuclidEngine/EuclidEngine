2ec65a0af0c9e66e148d16a7938423e50ee0472d
bardomania <52869617+bardomania@users.noreply.github.com>
2022-04-04 01:14:53 +0200

    (QF): Connection to Euclid Engine account
    Connection window and popup first commit

---
+++ Wrapper_Project/Assets/EuclidEngineConnectionWindow.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+class EuclidEngineConnection : EditorWindow
+{
+    private string username = "";
+    private string mdp = "";
+
+    private void SaveCredential(string user, string mdp)
+    {
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenWrite(destination);
+        else file = File.Create(destination);
+        Debug.Log(destination);
+
+        EuclidEngineData data = new EuclidEngineData(user, mdp);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    [MenuItem("Window/EuclidEngineConnection")]
+    public static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(EuclidEngineConnection));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Handle your EuclidEngine account connection from this window");
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        username = EditorGUILayout.TextField("Username: ", username);
+        mdp = EditorGUILayout.PasswordField("Password: ", mdp);
+        //buton to save credentials
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Log in"))
+        {
+            SaveCredential(username, mdp);
+        }
+        //buton to delete credentials
+        if (GUILayout.Button("Delete credentials"))
+        {
+            File.Delete(Application.persistentDataPath + "/save.dat");
+        }
+        EditorGUILayout.EndHorizontal();
+        this.Repaint();
+    }
+}
