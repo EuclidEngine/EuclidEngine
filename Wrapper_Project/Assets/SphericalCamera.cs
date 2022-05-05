5407cf54bd4e5f241f6a37f83ac3a43c23a3f820
Pival <valentin.l-homel@epitech.eu>
2022-05-05 11:38:36 +0300

    Commit

--- Wrapper_Project/Assets/SphericalCamera.cs
+++ Wrapper_Project/Assets/SphericalCamera.cs
@@ -1,34 +1,70 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEditor;
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class SphericalCamera : MonoBehaviour
 {
     public Material shaderHolder;
+    public EuclidEngineSpace EESS;
 
     private Vector3 oldPos;
-    private Vector4 position4;
+    private Quaternion oldRot;
 
-    // Start is called before the first frame update
+    public Vector4 pos4;
+    private Quaternion rot4;
+
     void Start()
     {
+        EESS = GameObject.FindObjectOfType<EuclidEngineSpace>();
+        print((EESS, EESS.space));
         oldPos = transform.position;
-        position4 = new Vector4(oldPos.x, oldPos.y, oldPos.z, 1f);
-        position4 = new Vector4(0,0,0,-1f) + (-2*Vector4.Dot(new Vector4(oldPos.x, oldPos.y, oldPos.z, 2f), new Vector4(0,0,0,-1f))/Vector4.Dot(new Vector4(oldPos.x, oldPos.y, oldPos.z, 2f),new Vector4(oldPos.x, oldPos.y, oldPos.z, 2f))) * new Vector4(oldPos.x, oldPos.y, oldPos.z, 2f);
+        oldRot = transform.rotation;
+        pos4 = new Vector4(0, 0, 0, EESS.radius);
+        rot4 = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        Vector3 scale = transform.lossyScale;
+        if (oldRot != transform.rotation) {
+            EuclidEngineSpace.EESphericalSpace_rotateObject(EESS.space,
+                ref pos4, ref rot4, ref scale,
+                Quaternion.RotateTowards(oldRot, transform.rotation, 180));
+            oldRot = transform.rotation;
+        }
+        if (oldPos != transform.position) {
+            EuclidEngineSpace.EESphericalSpace_moveObject(EESS.space,
+                ref pos4, ref rot4, ref scale,
+                transform.position - oldPos);
+            Vector4 pos3 = pos4;
+            Quaternion rot3 = rot4;
+            
+            print(String.Format("While transposing [{0:G9},{1:G9},{2:G9},{3:G9}] => {4:G9}\n", pos3.x, pos3.y, pos3.z, pos3.w, pos3.sqrMagnitude-1));
+            EuclidEngineSpace.EESphericalSpace_to3D(EESS.space,
+                ref pos3, ref rot3, ref scale, new Vector4(0, 0, 0, EESS.radius));
+            transform.position = oldPos = pos3;
+            transform.rotation = oldRot = rot3;
+            
+            shaderHolder.SetVector("_Camera", pos4);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Vector3 delta = transform.position - oldPos;
-        oldPos = transform.position;
-        position4 = new Vector4(
-            position4.x * Mathf.Cos(delta.x) + position4.w * Mathf.Sin(delta.x),
-            position4.y * Mathf.Cos(delta.y) + position4.w * Mathf.Sin(delta.y),
-            position4.z * Mathf.Cos(delta.z) + position4.w * Mathf.Sin(delta.z),
-            position4.w * Mathf.Cos(delta.x) * Mathf.Cos(delta.y) * Mathf.Cos(delta.z)
-                - position4.x * Mathf.Sin(delta.x) - position4.y * Mathf.Sin(delta.y) - position4.z * Mathf.Sin(delta.z)
-        );
-        shaderHolder.SetVector("_Camera", position4);
-    }
+    //void Update()
+    //{
+    //    print(transform.localRotation);
+    //    Vector3 delta = transform.position - oldPos;
+    //    oldPos = transform.position;
+    //    position4 = new Vector4(
+    //        position4.x * Mathf.Cos(delta.x) + position4.w * Mathf.Sin(delta.x),
+    //        position4.y * Mathf.Cos(delta.y) + position4.w * Mathf.Sin(delta.y),
+    //        position4.z * Mathf.Cos(delta.z) + position4.w * Mathf.Sin(delta.z),
+    //        position4.w * Mathf.Cos(delta.x) * Mathf.Cos(delta.y) * Mathf.Cos(delta.z)
+    //            - position4.x * Mathf.Sin(delta.x) - position4.y * Mathf.Sin(delta.y) - position4.z * Mathf.Sin(delta.z)
+    //    );
+    //}
 }
--- Wrapper_Project/Assets/SphericalObject.cs
+++ Wrapper_Project/Assets/SphericalObject.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class SphericalObject : MonoBehaviour
 {
+    public Material shaderHolder;
+    public EuclidEngineSpace EESS;
+    private SphericalCamera cam;
+
+    private Vector3 oldPos;
+    private Quaternion oldRot;
+
+    private Vector4 hyperorigin;
+    private Vector4 pos4;
+    private Quaternion rot4;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EESS = GameObject.FindObjectOfType<EuclidEngineSpace>();
+        cam = GameObject.FindObjectOfType<SphericalCamera>();
+
+        EESS.to4D(transform.position, transform.rotation, hyperorigin, ref pos4, ref rot4);
+        EESS.to3D(pos4, rot4, cam, ref oldPos, ref oldRot);
+
+        transform.position = oldPos;
+        transform.rotation = oldRot;
     }
 
-    private Vector4 hyperorigin;
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.rotation != oldRot)
+            EESS.rotate(ref pos4, ref rot4, Quaternion.RotateTowards(oldRot, transform.rotation, 180));
+        if (transform.position != oldPos)
+            EESS.move(ref pos4, ref rot4, transform.position - oldPos);
+        if (transform.rotation != oldRot || transform.position != oldPos) {
+            //print(String.Format("While transposing [{0:G9},{1:G9},{2:G9},{3:G9}] => {4:G9}", oldPos.x, oldPos.y, oldPos.z, pos3.sqrMagnitude-1));
+            EESS.to3D(pos4, rot4, cam, ref oldPos, ref oldRot);
+            transform.position = oldPos;
+            transform.rotation = oldRot;
+        }
     }
 
     public void SetHyperOrigin(Vector4 _hyperorigin)
@@ -26,75 +53,5 @@ public class SphericalObject : MonoBehaviour
         rend.GetPropertyBlock(matProp);
         matProp.SetVector("_TileOrigin", hyperorigin);
         rend.SetPropertyBlock(matProp);
-    }
-
-    // Old version, slightly off. Not used anymore
-    private Vector4 StereographicProjection()
-    {
-        /*const*/ Vector4 pos = transform.position; pos.w = 1;
-        /*const*/ Vector4 sphereCenter = new Vector4(0,0,0,0);
-        /*const*/ Vector4 planeNormal = hyperorigin - sphereCenter;
-        /*const*/ float sphereRadius = 5;
-
-        /*const*/ Vector4 calcSphereCenter = new Vector4(0,0,0,sphereRadius);
-
-        Vector4 Q = calcSphereCenter + new Vector4(0,0,0,1) * sphereRadius;
-        Vector4 QP = pos - Q;
-        Vector4 SQ = Q - calcSphereCenter;
-        float A = Vector4.Dot(QP, QP);
-        float B = 2 * Vector4.Dot(QP, SQ);
-        float k = -B / A;
-
-        Vector4 projPoint = sphereCenter - (planeNormal.normalized * sphereRadius - sphereCenter);
-        return projPoint + k * QP;
-    }
-
-    private Vector4 ReverseStereographicProjection()
-    {
-        /*const*/ Vector4 pos = transform.position; pos.w = 1;
-        /*const*/ Vector4 sphereCenter = new Vector4(0,0,0,0);
-        /*const*/ Vector4 planeNormal = hyperorigin - sphereCenter;
-        /*const*/ float sphereRadius = 5;
-
-        /*const*/ Vector4 calcSphereCenter = new Vector4(0,0,0,sphereRadius);
-
-        Vector4 Q = calcSphereCenter + new Vector4(0,0,0,1) * sphereRadius;
-        Vector4 QP = pos - Q;
-        Vector4 SQ = Q - calcSphereCenter;
-        float A = Vector4.Dot(QP, QP);
-        float B = 2 * Vector4.Dot(QP, SQ);
-        float k = -B / A;
-
-        Vector4 projPoint = sphereCenter - (planeNormal.normalized * sphereRadius - sphereCenter);
-        Vector4 rotVec1 = SQ;
-        Vector4 rotVec2 = projPoint - sphereCenter;
-        Matrix4x4 rotMat = Matrix4x4.identity;
-
-        if ((rotVec1.x == 0 && rotVec1.y == 0 && rotVec2.x == 0 && rotVec2.y == 0) ||
-            ((rotVec1.x != 0 || rotVec1.y != 0) && (rotVec2.x != 0 || rotVec2.y != 0))) {
-                float angle1 = Vector2.SignedAngle(new Vector2(rotVec1.x, rotVec1.y), new Vector2(rotVec2.x, rotVec2.y)) * Mathf.Deg2Rad;
-                float angle2 = Vector2.SignedAngle(new Vector2(rotVec1.z, rotVec1.w), new Vector2(rotVec2.z, rotVec2.w)) * Mathf.Deg2Rad;
-                rotMat[0,0] = Mathf.Cos(angle1); rotMat[1,1] = Mathf.Cos(angle1);
-                rotMat[0,1] =-Mathf.Sin(angle1); rotMat[1,0] = Mathf.Sin(angle1);
-                rotMat[2,2] = Mathf.Cos(angle2); rotMat[3,3] = Mathf.Cos(angle2);
-                rotMat[2,3] =-Mathf.Sin(angle2); rotMat[3,2] = Mathf.Sin(angle2);
-        } else if ((rotVec1.x == 0 && rotVec1.z == 0 && rotVec2.x == 0 && rotVec2.z == 0) ||
-            ((rotVec1.x != 0 || rotVec1.z != 0) && (rotVec2.x != 0 || rotVec2.z != 0))) {
-                float angle1 = Vector2.SignedAngle(new Vector2(rotVec1.x, rotVec1.z), new Vector2(rotVec2.x, rotVec2.z)) * Mathf.Deg2Rad;
-                float angle2 = Vector2.SignedAngle(new Vector2(rotVec1.y, rotVec1.w), new Vector2(rotVec2.y, rotVec2.w)) * Mathf.Deg2Rad;
-                rotMat[0,0] = Mathf.Cos(angle1); rotMat[2,2] = Mathf.Cos(angle1);
-                rotMat[0,2] =-Mathf.Sin(angle1); rotMat[2,0] = Mathf.Sin(angle1);
-                rotMat[1,1] = Mathf.Cos(angle2); rotMat[3,3] = Mathf.Cos(angle2);
-                rotMat[1,3] =-Mathf.Sin(angle2); rotMat[3,1] = Mathf.Sin(angle2);
-        } else if ((rotVec1.x == 0 && rotVec1.w == 0 && rotVec2.x == 0 && rotVec2.w == 0) ||
-            ((rotVec1.x != 0 || rotVec1.w != 0) && (rotVec2.x != 0 || rotVec2.w != 0))) {
-                float angle1 = Vector2.SignedAngle(new Vector2(rotVec1.x, rotVec1.w), new Vector2(rotVec2.x, rotVec2.w)) * Mathf.Deg2Rad;
-                float angle2 = Vector2.SignedAngle(new Vector2(rotVec1.y, rotVec1.z), new Vector2(rotVec2.y, rotVec2.z)) * Mathf.Deg2Rad;
-                rotMat[0,0] = Mathf.Cos(angle1); rotMat[3,3] = Mathf.Cos(angle1);
-                rotMat[0,3] =-Mathf.Sin(angle1); rotMat[3,0] = Mathf.Sin(angle1);
-                rotMat[1,1] = Mathf.Cos(angle2); rotMat[2,2] = Mathf.Cos(angle2);
-                rotMat[1,2] =-Mathf.Sin(angle2); rotMat[2,1] = Mathf.Sin(angle2);
-        }
-        return rotMat * (SQ + k * QP) + sphereCenter;
     }
 }
